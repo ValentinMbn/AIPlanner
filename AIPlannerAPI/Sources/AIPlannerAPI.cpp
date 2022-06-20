87e4a6cffca9a35647b32852bb39dea6db97461d
ValentinMbn <valentin.maubian@gmail.com>
2022-06-20 17:24:20 +0900

    [Add] Cmake project

---
+++ AIPlannerAPI/Sources/AIPlannerAPI.cpp
@@ -0,0 +1,12 @@
+﻿// AIPlannerAPI.cpp : définit le point d'entrée de l'application.
+//
+
+#include "AIPlannerAPI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
